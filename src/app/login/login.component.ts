d6ba7b124100b1292945fae5ed8dd1ce6103f1c6
Tamas Piski <tpiski@gmail.com>
2019-11-11 14:46:48 +0100

    login phase2

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,21 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  user : string;
-  password : string;
-  someArray: {key: string, value: string}[] = [
+  userInput : any;
+  passwordInput : string;
+  user : any;
+  Users: {key: string, value: string}[] = [
     {key: 'Sandra', value: 'password'},
     {key: 'Tamas', value: 'wordpass'}
   ];
+
+  constructor() {}
 
   ngOnInit() {
   }
   
   login() {
-    this.user = document.getElementById("user").innerText;
-    this.password = document.getElementById("pass").innerText;
-    console.log(this.user)
-    console.log(this.password)
+    this.userInput = document.getElementById("user").value;
+    this.passwordInput = document.getElementById("pass").value;
+    console.log(this.userInput)
+    console.log(this.passwordInput)
+    for (let user of this.Users) {
+      console.log(user)
+      if (this.userInput == user.key) {}
+    }
   }
 
 
