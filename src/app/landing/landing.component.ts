cb2426d2ea39065383c7a1951eda35200c2eb537
Ece Czak <ececzak@gmail.com>
2019-11-12 10:24:04 +0100

    Update landing.component.ts

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactsService } from "../shared/contacts.service";
 
 @Component({
   selector: 'app-landing',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
+	contactsArray =[];
+	showDeletedMessage : boolean;
+    searchText:string = "";
 
-  constructor() { }
+  constructor(private contactsService: ContactsService) { }
 
-  ngOnInit() {
-  }
+  // keeps the list updated
+	  ngOnInit() {
+		    this.contactsService.getContacts().subscribe(
+            (list) => {
+                this.contactsArray = list.map( (item) => {
+                    return {
+                        $key : item.key,
+                        ...item.payload.val()
+                    }
+                })
+            }
+        );
+  	}
+  	// search function for name (both first and last name) and type
+    filterCondition(contact){
+        return (contact.firstName.toLowerCase().indexOf(this.searchText.toLowerCase()) 
+                && contact.lastName.toLowerCase().indexOf(this.searchText.toLowerCase()) 
+                && contact.type.toLowerCase().indexOf(this.searchText.toLowerCase())) != -1 ;
+    }
 
 }
