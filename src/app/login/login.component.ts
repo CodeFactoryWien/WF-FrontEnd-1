70bc09a06187e36c7b481fd8d6a9551001029078
SG <web.sg@protonmail.com>
2019-11-13 12:01:24 +0100

    changes login/logout functionality (service added); adds greeting for authenticated users

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,58 +1,39 @@
-
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { LoginService  } from "../shared/login.service";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
 
-  
-  userInput : any;
-  passwordInput : any;
-  user : any;
-  button : any;
-  button2: any;
-
-  /* User names and passwords */
-  users = [
-    {key: 'Sandra', value: 'password'},
-    {key: 'Tamas', value: 'wordpass'},
-    {key: 'Ece', value: 'boss'},
-  ];
+  	/* User names and passwords */
+  	users = [
+    	{key: 'Sandra', value: 'password'},
+    	{key: 'Tamas', value: 'wordpass'},
+    	{key: 'Ece', value: 'boss'},
+  	];
     
 
-  constructor(private router: Router) {}
+  	constructor(private loginService: LoginService) {}
     
-  ngOnInit() {
-  }
+  	ngOnInit() {
+  	}
   
-  onSubmit(e) {
+	onSubmit(e) {
     /* This sets the login status to true in case the user name and password match and console logs the login message
     or the wrong password message*/
-    localStorage.setItem("login", "false");
-    this.userInput = (<HTMLInputElement>document.getElementById("user"));
-    this.passwordInput = (<HTMLInputElement>document.getElementById("pass"));
-    for (let user of this.users) {
-        if (this.userInput.value == user.key && this.passwordInput.value == user.value) {
-            localStorage.setItem("login", "true"); 
-            localStorage.setItem("user", user.key); 
-            document.getElementById("log").innerHTML = `
-            <a id="login-button" routerLink="/" class="nav-link js-scroll-trigger" href="">Logout</a>`
-            document.getElementById("login-button").addEventListener("click", function() {
-              localStorage.setItem("login", "false");
-              console.log("in");
-              document.getElementById("log").innerHTML = `<a id="login-button" routerLink="/login" 
-                                            class="nav-link js-scroll-trigger" href="">Login</a>`
-          })
-            this.router.navigate(['contacts']);       
-        }
-        else {
-            document.getElementById("message").innerText = "Wrong password or username";
-        }
-    }
-  }
-    
+    	let userInput = (<HTMLInputElement>document.getElementById("user"));
+    	let passwordInput = (<HTMLInputElement>document.getElementById("pass"));
+    	for (let user of this.users) {
+        	if (userInput.value == user.key && passwordInput.value == user.value) {
+            	this.loginService.userLogin(user);       
+        	}
+        	else {
+            	document.getElementById("message").innerText = "Wrong password or username";
+        	}
+    	}
+  	}
 }
