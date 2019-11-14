e9f4596923ec9d07531bbfac02a6f86ba5821544
Ece Czak <ececzak@gmail.com>
2019-11-14 11:21:25 +0100

    prepare file for deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+    production: true,
+    firebaseConfig : {
+        apiKey: "AIzaSyCGt4x-Hsx9QdincmZdl4-JsdNUzVnRdXY",
+        authDomain: "contacts-e5ef7.firebaseapp.com",
+        databaseURL: "https://contacts-e5ef7.firebaseio.com",
+        projectId: "contacts-e5ef7",
+        storageBucket: "contacts-e5ef7.appspot.com",
+        messagingSenderId: "494211130030",
+        appId: "1:494211130030:web:2e966eac6e0ab4c412433f"
+    }
 };
